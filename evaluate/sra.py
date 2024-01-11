590c114c21066b3f588684bbbc642e06dc2c204f
shiv2398 <sahityahavi111@gmail.com>
2024-01-11 12:23:29 +0530

    .

--- evaluate/sra.py
+++ evaluate/sra.py
@@ -29,3 +29,33 @@ def frame_sra(input_, output_, sequence_length):
             sra_list.append(sa)
     return sra_list
 
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+# Assuming you have a list of 240 points (replace data_list with your actual list)
+data_list = np.load('Arrest_001_sr_.npy')
+
+# Initialize the plot
+fig, ax = plt.subplots()
+line, = ax.plot(data_list, label='Data')
+
+ax.set_xlim(0, len(data_list))
+ax.set_ylim(0, 2)
+ax.set_xlabel('frame(t)')
+ax.set_ylabel('SignularityScore(sr)')
+ax.set_yticks(np.arange(0.0,2, 0.2).tolist())
+ax.legend(loc='lower right')
+
+# Function to update the plot in each iteration
+def update_plot(i):
+    # Update the data in the plot
+    line.set_data(range(i), data_list[:i])
+
+    return line,
+
+# Create animation
+ani = FuncAnimation(fig, update_plot, frames=len(data_list), interval=100, blit=True)
+plt.savefig('arrest_001')
+# Display the plot
+plt.show()
